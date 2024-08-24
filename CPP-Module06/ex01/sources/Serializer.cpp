315a176ac0ad0f9bd208296de2e087f84e0889aa
Davi Nunes <davimeirelespn@gmail.com>
2024-08-24 13:50:24 +0100

    cpp06 changes

--- CPP-Module06/ex01/sources/Serializer.cpp
+++ CPP-Module06/ex01/sources/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: dmeirele <dmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 18:24:33 by dmeirele          #+#    #+#             */
-/*   Updated: 2024/08/12 18:24:49 by dmeirele         ###   ########.fr       */
+/*   Updated: 2024/08/24 13:48:52 by dmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ Serializer &Serializer::operator=(const Serializer &rhs)
 
 uintptr_t Serializer::serialize(Data* ptr)
 {
-	uintptr_t raw = reinterpret_cast<uintptr_t>(new Data(*ptr));
+	uintptr_t raw = reinterpret_cast<uintptr_t>(ptr);
 	return (raw);
 }
 
--- CPP-Module06/ex02/sources/main.cpp
+++ CPP-Module06/ex02/sources/main.cpp
@@ -6,11 +6,12 @@
 /*   By: dmeirele <dmeirele@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 18:32:46 by dmeirele          #+#    #+#             */
-/*   Updated: 2024/08/12 18:34:56 by dmeirele         ###   ########.fr       */
+/*   Updated: 2024/08/24 13:50:10 by dmeirele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Base.hpp"
+#include "../includes/includes.hpp"
 
 int main()
 {
@@ -18,13 +19,13 @@ int main()
 	Base *nulPtr = 0;
 	base = generate();
 
-	std::cout << "\033[32m" << "*** Identify Base With Pointer Paramater ***" << "\033[0m" << std::endl;
+	std::cout << Green << "*** Identify Base With Pointer Paramater ***" << Reset << std::endl;
 	identify(base);
 	std::endl(std::cout);
-	std::cout << "\033[32m" << "*** Identify Base With Reference Paramater ***" << "\033[0m" << std::endl;
+	std::cout << Green << "*** Identify Base With Reference Paramater ***" << Reset << std::endl;
 	identify(*base);
 	std::endl(std::cout);
-	std::cout << "\033[32m" << "*** Identify Base With Null Pointer Paramater ***" << "\033[0m" << std::endl;
+	std::cout << Green << "*** Identify Base With Null Pointer Paramater ***" << Reset << std::endl;
 	identify(nulPtr);
 
 	delete (base);
